cdc782be533b23eadb5a848b3fcf276225cb8623
theandreeei <vertru112211@gmail.com>
2024-12-28 18:18:16 +0200

    Predicted hours&energy

--- main.py
+++ main.py
@@ -3,25 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-# Data
-data = {
-    'Days': [1, 2, 3, 4, 5],
-    'Candies': [2, 4, 6, 8, 10],
-}
 
-df = pd.DataFrame(data)
-print(df)
+# Генеруємо дані
+np.random.seed(24)
+hours = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+energy_usage = 10 * hours + 15 + np.random.normal(0, 5, len(hours))  # Додаємо шум
 
-# Visualisation of data
-plt.scatter(df['Days'], df['Candies'], color='blue')
-plt.title('Dependency graph')
-plt.xlabel('Days')
-plt.ylabel('Candies')
-# plt.show()
 
-# X days and Y candies
-X = df[['Days']]
-Y = df['Candies']
+# Вхідні та вихідні дані
+X = hours.reshape(-1, 1)  # Перетворюємо в 2D-масив
+Y = energy_usage  # Вихідні дані
+
 
 # Creating and learning of Model
 model = LinearRegression()
@@ -35,11 +27,11 @@ print('b', model.intercept_)
 Y_pred = model.predict(X)
 
 # Graph with the line of regression
-plt.scatter(X, Y, color='blue')
+plt.scatter(X, Y, color='blue', label='Real data')
 plt.plot(X, Y_pred, color='red')
 plt.title('Line regression')
-plt.xlabel('Days')
-plt.ylabel('Candies')
+plt.xlabel('Години')
+plt.ylabel('Витрати електроенергії')
 plt.show()
 
 # Forecast
